5928d7c1d384092cc9fc71b293c897db3f55a12c
DaniilCoolUkraine <cd0games2000@gmail.com>
2022-11-19 19:29:12 +0200

    added collision detection logic

--- Unavinar/Assets/Scripts/ColDetect.cs
+++ Unavinar/Assets/Scripts/ColDetect.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ColDetect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Obstacles"))
+            return;
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        //De-parent object
+        transform.parent = null;
         
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
     }
 }
